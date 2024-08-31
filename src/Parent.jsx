aa89db95d16426ae52584b648bb90715fd70fc2a
Abhi1-singh <abhisheksingh909844@gmail.com>
2024-08-31 16:43:38 +0530

    first commit

---
+++ src/Parent.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+// function Parient({props}) {
+//   return (
+//     <div>
+//       <h1>hello my name is{prop}</h1>
+//     </div>
+//   )
+// }
+
+// export default Parient
